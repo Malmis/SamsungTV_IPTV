8c4845fefe4fefd6d3e2aea12ccc1aea78d4a7e9
Kristoffer Malmström <mitod1@gmail.com>
2021-02-07 15:56:30 +0100

    Changed to mainly use Swedish and Norwegian free streams.

--- myiptv/app/javascript/Main.js
+++ myiptv/app/javascript/Main.js
@@ -8,11 +8,27 @@ var Main =
 };
 
 // put this into channels.js  (and host channel.js on your web server, see index.html)
-var channelList = 
+var channelList =
 		 [
-			{name:'ABC News', url:'http://abclive.abcnews.com/i/abc_live4@136330/index_1200_av-b.m3u8'},
-			{name:'Euro news', url:'http://ewns-hls-b-cdn.hexaglobe.net/1259b4185677ea20e8736c0f0182e14a/58fc8503/euronews_eng.smil/playlist.m3u8'}
-		 ];	
+			{name:'Aftonbladet', url:'https://aftonbladetlive-lh.akamaihd.net/i/livestream01_1@182509/master.m3u8'},
+			{name:'ATG', url:'https://httpcache0-00688-cacheliveedge0.dna.qbrick.com/00688-cacheliveedge0/out/u/atg_sdi_1_free_3.m3u8'},
+			{name:'Himlen TV', url:'http://vod.tv7.fi:1935/tv7-se/_definst_/mp4:tv7-se.stream_360p/playlist.m3u8'},
+			{name:'Kanal 10', url:'https://cdn-kanal10.crossnet.net/kanal10/mp4:mpegts.stream/playlist.m3u8'},
+			{name:'TV4', url:'https://csm-e-tv4se-eb.tls1.yospace.com/csm/live/142791716.m3u8'},
+			{name:'TV4 Nyheterna', url:'https://csm-e-tv4se-eb.tls1.yospace.com/csm/live/142791350.m3u8'},
+			{name:'TV4 Svensk Politik', url:'https://csm-e-tv4se-eb.tls1.yospace.com/csm/live/142791553.m3u8'},
+			{name:'DiTV SE', url:'https://httpcache0-03837-cachelive2.dna.qbrick.com/03837-cachelive2/smil:03837_tx4_720p/playlist.m3u8'},
+			{name:'Expressen TV SE', url:'https://httpcache0-03837-cachelive2.dna.qbrick.com/03837-cachelive2/smil:03837_tx2_720p/playlist.m3u8'},
+			{name:'SportExpressen SE', url:'https://httpcache0-03837-cachelive6.dna.qbrick.com/03837-cachelive6/smil:03837_tx6_720p/playlist.m3u8'},
+			{name:'Västmanlands TV', url:'https://edg01-prd-se-dcs.solidtango.com/edge/_lo9yf4l5_/lo9yf4l5/playlist.m3u8'},
+			{name:'Viasat History Russian', url:'https://s1.idata.uz/stch_temp20/tracks-v1a1/mono.m3u8'},
+			{name:'Viasat Sport', url:'https://persik.by/stream/2944/263250/10249.m3u8'},
+			{name:'MTV (Norge)', url:'https://unilivemtveu-lh.akamaihd.net/i/mtvno_1@346424/master.m3u8'},
+			{name:'TV Haugaland', url:'https://900c544ac47302fffd7d3f12b4c745fd-httpcache0-90216-cachelive0.dna.qbrick.com/90216-cachelive0/smil:APPLETV/playlist.m3u8'},
+			{name:'Yle TV1', url:'https://yletvhdliveworld-lh.akamaihd.net/i/yletv1hdworld_1@187592/master.m3u8'},
+			{name:'Yle TV2', url:'https://yletvhdliveworld-lh.akamaihd.net/i/yletv2hdworld_1@187593/master.m3u8'},
+			{name:'Öppna Kanalen', url:'http://83.140.64.214/edge/_451iw2h_/451iw2h/playlist.m3u8'}
+		 ];
 
 Main.onLoad = function()
 {
@@ -29,10 +45,10 @@ var onShowEvent = function() {
 	 // For volume OSD
 	 pluginAPI.unregistKey(tvKey.KEY_VOL_UP);
 	 pluginAPI.unregistKey(tvKey.KEY_VOL_DOWN);
-	 pluginAPI.unregistKey(tvKey.KEY_MUTE); 
-	  
+	 pluginAPI.unregistKey(tvKey.KEY_MUTE);
+
 	 sPlayer.init();
-	
+
 	setTimeout(function(){
 			sPlayer.gotoChannel(0);
 	}, 1000);
@@ -40,7 +56,7 @@ var onShowEvent = function() {
 
 Main.onUnload = function()
 {
-	
+
 };
 
 Main.enableKeys = function()
@@ -60,17 +76,17 @@ Main.keyDown = function()
 			break;
 		case tvKey.KEY_UP:
 			break;
-		case tvKey.KEY_DOWN:			
+		case tvKey.KEY_DOWN:
 			break;
 		case tvKey.KEY_ENTER:
-		case tvKey.KEY_PANEL_ENTER:			
+		case tvKey.KEY_PANEL_ENTER:
 			break;
 		case tvKey.KEY_CH_UP:
 			sPlayer.channelUp();
 			break;
 		case tvKey.KEY_CH_DOWN:
 			sPlayer.channelDown();
-			break;		
+			break;
 		case tvKey.KEY_0:
 			sPlayer.gotoChannel(0);
 			break;
@@ -78,7 +94,7 @@ Main.keyDown = function()
 			sPlayer.gotoChannel(1);
 			break;
 		case tvKey.KEY_2:
-			sPlayer.gotoChannel(2);		
+			sPlayer.gotoChannel(2);
 			break;
 		case tvKey.KEY_3:
 			sPlayer.gotoChannel(3);
